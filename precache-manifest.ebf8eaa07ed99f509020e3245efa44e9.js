bd81d4f37feb51e8a0e8a2101d78837bdadc6bbe
Viviana Davila <vdavila@ucsd.edu>
2019-11-18 15:40:51 -0800

    portfolio transfer

---
+++ precache-manifest.ebf8eaa07ed99f509020e3245efa44e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50469dd286d7128e4705d9680b68b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2645801c86fed2ec1362",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c8b86a33db80496c3e20",
+    "url": "/static/css/main.96f9b6cf.chunk.css"
+  },
+  {
+    "revision": "2645801c86fed2ec1362",
+    "url": "/static/js/2.e5c53a3a.chunk.js"
+  },
+  {
+    "revision": "c8b86a33db80496c3e20",
+    "url": "/static/js/main.537cfe4f.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "ca9669973e69cf0a8fa3696ead4e8a42",
+    "url": "/static/media/Viviana_Davila_Resume.ca966997.pdf"
+  },
+  {
+    "revision": "ca6c3c8a82d92f0d19808642b640c5dd",
+    "url": "/static/media/venn.ca6c3c8a.png"
+  },
+  {
+    "revision": "3b01648c1a645d81c561784d33421231",
+    "url": "/static/media/web_background.3b01648c.png"
+  }
+]);
