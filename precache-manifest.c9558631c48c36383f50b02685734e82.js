d7edc64b91932af838f70b1a16a6abdc47039d57
Viviana Davila <vdavila@ucsd.edu>
2020-01-15 10:12:27 -0800

    update

---
+++ precache-manifest.c9558631c48c36383f50b02685734e82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d363291a3226ab6af54514777d4d79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c923d19d638f1dbfa83",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "400231726f0dc2458836",
+    "url": "/static/css/main.660d104d.chunk.css"
+  },
+  {
+    "revision": "1c923d19d638f1dbfa83",
+    "url": "/static/js/2.57c98782.chunk.js"
+  },
+  {
+    "revision": "400231726f0dc2458836",
+    "url": "/static/js/main.dec20a0d.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "ca9669973e69cf0a8fa3696ead4e8a42",
+    "url": "/static/media/Viviana_Davila_Resume.ca966997.pdf"
+  },
+  {
+    "revision": "c47d5a565cc1e90379bf045ec02b1e5a",
+    "url": "/static/media/cssa.c47d5a56.pdf"
+  },
+  {
+    "revision": "ca6c3c8a82d92f0d19808642b640c5dd",
+    "url": "/static/media/venn.ca6c3c8a.png"
+  },
+  {
+    "revision": "3b01648c1a645d81c561784d33421231",
+    "url": "/static/media/web_background.3b01648c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d6479885013e26cafede16fccae3b1e.js"
+  "/precache-manifest.c9558631c48c36383f50b02685734e82.js"
 );
 
 self.addEventListener('message', (event) => {
